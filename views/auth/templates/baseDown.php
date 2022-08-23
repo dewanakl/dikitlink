6879e4421b766b1bb16259ae8cfcac652be8e692
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-08-23 10:06:46 +0700

    add sw.js

--- views/auth/templates/baseDown.php
+++ views/auth/templates/baseDown.php
@@ -1,3 +1,11 @@
+<script src="<?= asset('sw.js') ?>"></script>
+<script>
+    if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register('sw.js').then(function(reg) {
+            console.log('Service worker has been registered for scope: ' + reg.scope);
+        });
+    }
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
