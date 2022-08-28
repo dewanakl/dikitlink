1862231c8f9f9dc179c5748b3e4745530393c702
dewanakl <53048107+dewanakl@users.noreply.github.com>
2022-08-28 15:37:12 +0700

    db update

--- core/Database/DataBase.php
+++ core/Database/DataBase.php
@@ -61,7 +61,8 @@ class DataBase
 
         $option = [
             PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_SSL_CA => env('SSL_CERT_PATH'),
         ];
 
         try {
