b0020b02f929b497eb3b5f3bc9f019cc1e62cc04
dewanakl <dewanakretarta29@gmail.com>
2022-10-30 01:43:13 +0700

    email and css update

--- views/email/reset.php
+++ views/email/reset.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html>
+<?php parents('email/app') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+<?php section('main') ?>
 
-<body>
-    <h1 style="color: #126dff;"><?= ucfirst(env('APP_NAME')) ?></h1>
+<h3>Haii <strong><?= e($nama) ?></strong></h3>
+<p>Berikut link untuk pemulihan akun kamu :</p>
 
-    <h3>Haii <strong><?= e($nama) ?></strong></h3>
-    <p>Berikut link untuk pemulihan akun kamu :</p>
+<p><a href="<?= $link ?>" style="color: #126dff;" target="_blank" rel="noopener noreferrer"><?= e($link) ?></a></p>
+<p>Link reset password ini satu kali pakai, jika ini bukan kamu maka abaikan saja.</p>
 
-    <p><a href="<?= $link ?>" style="color: #126dff;" target="_blank" rel="noopener noreferrer"><?= e($link) ?></a></p>
-    <p>Link reset password ini satu kali pakai, jika ini bukan kamu maka abaikan saja.</p>
-
-    <hr />
-
-    <p>Salam hangat dari <?= env('APP_NAME') ?></p>
-    <p><a href="<?= asset('/') ?>" style="color: #126dff;"><?= e(parse_url(BASEURL, PHP_URL_HOST)) ?></a></p>
-</body>
-
-</html>
+<?php endsection('main') ?>
